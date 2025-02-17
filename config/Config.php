63680d3b1cafd14867f280f641f3548ddb34a303
DoomBreaker0 <isaiivan19@gmail.com>
2025-02-17 00:32:32 -0500

    Primer Commit

---
+++ config/Config.php
@@ -0,0 +1,14 @@
+<?php
+define('BASE_URL', 'http://192.168.100.22/pos/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'pos');
+define('CHARSET', '');
+define('TITLE', 'POS VENTA');
+define('MONEDA', '$/ ');
+define('HOST_SMTP', 'smtp.gmail.com');
+define('USER_SMTP', 'lovenaju2@gmail.com');
+define('CLAVE_SMTP', 'ikhnyjislfpyzhgj');
+define('PUERTO_SMTP', 465);
+?>
